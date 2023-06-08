c742617f5fe9ef99734f2ba9be272d4bf6b5b9c1
Jukon3234 <zzz971504@gmail.com>
2023-06-09 00:23:18 +0800

    add bat

---
+++ Release_Build.bat
@@ -0,0 +1,16 @@
+cd UI\Homepage
+pyuic5.exe -o ui_MainUI.py .\MainUI.ui
+pyuic5.exe -o ui_Help.py .\Help.ui
+pyuic5.exe -o ui_Setting.py .\Setting.ui
+cd ..
+cd ..
+
+cd systemdata\icon
+pyrcc5.exe -o ICON.py .\ICON.qrc
+cd ..
+cd ..
+pyinstaller --onefile --noconsole --icon=myicon.ico .\main.py
+#python.exe -B .\main.py
+
+
+
